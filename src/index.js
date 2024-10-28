7ef0f6523d38ab298661aa5f25c2d5858f5194b8
Maria <mshevchun@bk.ru>
2024-10-28 09:33:53 +0300

    final

--- src/index.js
+++ src/index.js
@@ -1,59 +1,58 @@
 import './index.css'
 // import jpg from './images/image.jpg'
-document.addEventListener ("DOMContentLoaded", function(){
-    const container = document.querySelector (".cursor-trail");
-    let imageIndex = 1;
-    let animationTimeout = null;
-    let currenlyPlaying = false;
+// document.addEventListener("DOMContentLoaded", function() {
+//   const container = document.querySelector(".cursor-trail");
+//   let imageIndex = 1;
+//   let animationTimeout = null;
+//   let currenlyPlaying = false;
 
-    function addNewItem(x, y) {
-        let newItem = document.createElement("div");
-        document.createElement ("div");
-        newItem.className = "item";
-        newItem.style.left = `${x - 75}px`;
-        newItem.style.top = `${y - 100}px`;
+//   function addNewItem(x, y) {
+//     let newItem = document.createElement("div");
+//     newItem.className = "item";
+//     newItem.style.left = `${x - 75}px`;
+//     newItem.style.top = `${y - 100}px`;
 
-        const img = document.createElement("img");
-        img.src = `./src/images/animatiom_try${imageIndex}.jpg`;
-        newItem.appendChild(img);
-        imageIndex = (imageIndex % 15) + 1;
+//     const img = document.createElement("img");
+//     img.src = `images/animatiom_try${imageIndex}.jpg`; // Исправленный путь
+//     newItem.appendChild(img);
+//     imageIndex = (imageIndex % 15) + 1;
 
-        container.appendChild(newItem);
-        manageItemLimit();
-    }
+//     container.appendChild(newItem);
+//     manageItemLimit();
+//   }
 
-    function manageItemLimit() {
-        while (container.children.length > 20) {
-            container.removeChild(container.firstChild);
-        }
-    }
+//   function manageItemLimit() {
+//     while (container.children.length > 20) {
+//       container.removeChild(container.firstChild);
+//     }
+//   }
 
-    function startAnimation() {
-        if (currenlyPlaying || container.children.length === 0) return;
-        currenlyPlaying = true;
+//   function startAnimation() {
+//     if (currenlyPlaying || container.children.length === 0) return;
+//     currenlyPlaying = true;
 
-        gsap.to(".item", {
-            y: 1000,
-            scale: 0.5,
-            opasity: 0,
-            duration: 0.5,
-            stagger: 0.025,
-            onComplete: function() {
-                this.targets().forEach((item) => {
-                    if (item.parentNode) {
-                        item.parentNode.removeChild(item);
-                    }
-                });
+//     gsap.to(".item", {
+//       y: 1000,
+//       scale: 0.5,
+//       opacity: 0, // Исправлено: opacity
+//       duration: 0.5,
+//       stagger: 0.025,
+//       onComplete: function() {
+//         this.targets().forEach((item) => {
+//           if (item.parentNode) {
+//             item.parentNode.removeChild(item);
+//           }
+//         });
 
-                currenlyPlaying = false;
-            },
-        });
-    }
-    container.addEventListener ("mousemove", function(event){
-        clearTimeout (animationTimeout);
-        addNewItem(event.pageX, event.pageY);
-        animationTimeout = setTimeout (startAnimation, 100);
+//         currenlyPlaying = false;
+//       },
+//     });
+//   }
 
-    });
+//   container.addEventListener("mousemove", function(event) {
+//     clearTimeout(animationTimeout);
+//     addNewItem(event.pageX, event.pageY);
+//     animationTimeout = setTimeout(startAnimation, 100);
+//   });
 
-});
+// });
