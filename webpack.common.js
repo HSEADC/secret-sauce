525eb7798a1894308f7d1e38d5677d408797430f
Maria <mshevchun@bk.ru>
2024-12-17 12:57:00 +0300

    fix

--- webpack.common.js
+++ webpack.common.js
@@ -8,7 +8,6 @@ const path = require('path');
 module.exports = {
   entry: {
     index: './src/index.js',
-    styleguide: './src/javascripts/styleguide.js',
   },
   output: {
     filename: '[name].js',
