87ad90876bcb009382ec9b1e3127222605c16b26
Vika <vikushayam@gmail.com>
2025-03-21 22:03:02 +0300

    changed main page

--- webpack.common.js
+++ webpack.common.js
@@ -9,11 +9,12 @@ module.exports = {
   entry: {
     index: './src/index.js',
     swiper: './src/javascript/swiper.js',
+    tab: './src/javascript/tab.js'
   },
   output: {
     filename: '[name].js',
     path: path.resolve(__dirname, 'docs'),
-    clean: true,
+    clean: true
   },
   module: {
     rules: [
@@ -24,9 +25,9 @@ module.exports = {
           loader: 'babel-loader',
           options: {
             presets: ['@babel/preset-env', '@babel/preset-react'],
-            plugins: ['@babel/plugin-proposal-class-properties'],
-          },
-        },
+            plugins: ['@babel/plugin-proposal-class-properties']
+          }
+        }
       },
       {
         test: /\.(sa|sc|c)ss$/i,
@@ -37,74 +38,74 @@ module.exports = {
             loader: 'postcss-loader',
             options: {
               postcssOptions: {
-                plugins: [['postcss-preset-env']],
-              },
-            },
+                plugins: [['postcss-preset-env']]
+              }
+            }
           },
-          'sass-loader',
-        ],
+          'sass-loader'
+        ]
       },
       {
         test: /\.html$/i,
-        loader: 'html-loader',
+        loader: 'html-loader'
       },
       {
         resourceQuery: /raw/,
-        type: 'asset/source',
+        type: 'asset/source'
       },
       {
         test: /\.(png|jpg|jpeg|gif|svg|webp)$/i,
         type: 'asset/resource',
         generator: {
-          filename: 'images/[hash][ext][query]',
-        },
+          filename: 'images/[hash][ext][query]'
+        }
       },
       {
         test: /\.(ttf|otf|woff|woff2)$/i,
         type: 'asset/resource',
         generator: {
-          filename: 'fonts/[hash][ext][query]',
-        },
-      },
-    ],
+          filename: 'fonts/[hash][ext][query]'
+        }
+      }
+    ]
   },
   plugins: [
     new MiniCssExtractPlugin({
       filename: '[name].css',
-      chunkFilename: '[id].css',
+      chunkFilename: '[id].css'
     }),
 
     // index
     new HtmlWebpackPlugin({
       template: './src/index.html',
       filename: './index.html',
-      chunks: ['index', 'swiper'],
+      chunks: ['index', 'swiper', 'tab']
     }),
 
     // все страницы разделов
     new HtmlWebpackPlugin({
       template: './src/articles.html',
       filename: './articles.html',
-      chunks: ['index'],
+      chunks: ['index']
     }),
 
     new HtmlWebpackPlugin({
       template: './src/about.html',
       filename: './about.html',
-      chunks: ['index'],
+      chunks: ['index']
     }),
 
     new HtmlWebpackPlugin({
       template: './src/newsletter.html',
       filename: './newsletter.html',
-      chunks: ['index'],
+      chunks: ['index']
     }),
 
     new HtmlWebpackPlugin({
       template: './src/styleguide.html',
       filename: './styleguide.html',
       chunks: ['index'],
-      chunks: ['index'],
+      chunks: ['index']
     }),
 
     // публикации в разделе "статьи" (articles)
@@ -124,8 +125,8 @@ module.exports = {
         path: path.join(__dirname, './src/partials/analytics.html'),
         location: 'analytics',
         template_filename: '*',
-        priority: 'replace',
-      },
-    ]),
-  ],
-};
+        priority: 'replace'
+      }
+    ])
+  ]
+}
