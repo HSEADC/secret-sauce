b9480d1552b734f29933fd0ecb41794cc8cc9dfd
Vika <vikushayam@gmail.com>
2025-04-15 10:32:58 +0300

    search vika

--- src/javascript/search-vanilla.js
+++ src/javascript/search-vanilla.js
@@ -2,9 +2,125 @@ import { getPostTeasers } from './Search-data';
 
 document.addEventListener('DOMContentLoaded', () => {
   getPostTeasers().then((data) => {
-    update(data);
-  });
-});
+    update(data)
+  })
+})
+
+document.addEventListener('DOMContentLoaded', () => {
+  getPostTeasers().then((data) => {
+    content = data
+    // createCard(content)
+    initSearch()
+  })
+})
+
+function initSearch() {
+  const O_Search = document.querySelector('.O_Search')
+  const A_SearchButton = O_Search.querySelector('.A_SearchButton')
+  const A_SearchInput = O_Search.querySelector('.A_SearchInput')
+
+  let requestText = getSearchRequest()
+
+  if (requestText != undefined) {
+    A_SearchInput.value = requestText
+
+    if (content) {
+      searchContent(requestText)
+    }
+  } else {
+    A_SearchInput.value = ''
+  }
+
+  A_SearchInput.addEventListener('input', (e) => {
+    requestText = e.target.value
+
+    if (requestText.length >= 3) {
+      A_SearchButton.classList.remove('disable')
+    } else {
+      A_SearchButton.classList.add('disable')
+    }
+  })
+
+  A_SearchInput.addEventListener('keydown', (e) => {
+    requestText = e.target.value
+
+    if (e.key === 'Enter') {
+      setSearchRequest(requestText)
+      searchContent(requestText)
+    }
+  })
+
+  A_SearchButton.addEventListener('click', (e) => {
+    if (!e.target.classList.contains('disable')) {
+      requestText = A_SearchInput.value
+      setSearchRequest(requestText)
+      searchContent(requestText)
+    }
+  })
+}
+
+function setSearchRequest(requestText) {
+  const url = window.location.href.split('?')[0]
+
+  window.location.href = url + '?request=' + requestText
+}
+
+function getSearchRequest() {
+  const url = new URL(window.location.href)
+  const searchParams = new URLSearchParams(url.search)
+
+  if (searchParams.has('request')) {
+    return searchParams.get('request')
+  }
+}
+
+function searchContent(requestText) {
+  const container = document.querySelector('.S_Content')
+  container.innerHTML = ''
+
+  let contentItemIds = []
+
+  content.forEach((contentItem) => {
+    const nbspRegEx = /[\u202F\u00A0]/gm
+    const punctuationRegEx = /[.,\/#!$%\^&\*;:{}=\-_`()]/gm
+
+    let { title, description, id } = contentItem
+
+    title = title.replaceAll(nbspRegEx, ' ')
+    title = title.replaceAll(punctuationRegEx, '')
+
+    description = description.replaceAll(nbspRegEx, ' ')
+    description = description.replaceAll(punctuationRegEx, '')
+
+    if (requestText.length >= 3) {
+      if (title.includes(requestText) || description.includes(requestText)) {
+        contentItemIds.push(id)
+      }
+    }
+  })
+
+  if (contentItemIds.length > 0) {
+    renderCardsByIds(container, contentItemIds)
+  } else {
+    renderNothingFounded(container)
+  }
+}
+
+function renderNothingFounded(container) {
+  container.innerHTML = 'Ничего не найдено :('
+}
+
+function renderCardsByIds(container, ids) {
+  ids = [...new Set(ids)]
+
+  ids.forEach((id) => {
+    content.forEach((item) => {
+      if (item.id == id) {
+        createCard(item)
+      }
+    })
+  })
+}
 
 function update(content) {
   const container = document.querySelector('.S_Content');
